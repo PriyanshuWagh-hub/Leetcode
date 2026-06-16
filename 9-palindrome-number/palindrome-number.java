78233bf656fb4a6e4e4a528ae49b6bbbf4d6064b
Priyanshu Wagh <waghpriyanshu@gmail.com>
2026-06-16 23:15:39 +0530

    Time: 7 ms (13.93%) | Memory: 46.3 MB (6.83%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        String str = String.valueOf(x);
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
+
+// class Solution {
+//     public boolean isPalindrome(int x) {
+//         // Negative numbers are not palindromes
+//         if (x < 0) {
+//             return false;
+//         }
+
+//         int original = x;
+//         int reverse = 0;
+
+//         while (x != 0) {
+//             int digit = x % 10;
+//             reverse = reverse * 10 + digit;
+//             x /= 10;
+//         }
+
+//         return original == reverse;
+//     }
+// }
